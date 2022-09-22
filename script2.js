f82dc16007682fe64f7b6177c5d1035f3a423ee7
isabuge09 <101752218+isabuge09@users.noreply.github.com>
2022-09-22 17:27:32 -0300

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,38 +1,41 @@
+let tam = 2 
+let titulo 
+for (i = 1; i < 7; i++) {
+    titulo = document.querySelector(`h${i}`) 
+    if (titulo != null) {
+        f_criaBotao() 
+        break
+    }
+} 
 
-let titulo
-let tamanho = 2
-for (let i = 1; i < 7; i++) {
-  titulo = document.querySelector(`h${i}`)
-  if (titulo != null) {
-    criarbotao()
-    break
-  }
-}
-function criarbotao() {
-  const botaoAumentar = document.createElement(null)
-  botaoAumentar.innerHTML = `<button type='button' onclick="clicou('mais')" id='botao'>+</button>`
+function f_criaBotao() {
+    const botaoMais = document.createElement(null) 
+    botaoMais.innerHTML = `<button id="botaomais"> + </button>` 
+    botaoMais.style.borderRadius = '10px'
+    botaoMais.style.width = '30px'
 
-  const botaoDiminuir = document.createElement(null)
-  botaoDiminuir.textContent = "-"
-  botaoDiminuir.innerHTML = `<button type='button' onclick="clicou('menos')" id='botao'>-</button>`
+    const botaoMenos = document.createElement(null) 
+    botaoMenos.innerHTML = `<button id="botaomenos"> - </button>`
+    botaoMenos.style.borderRadius = '10px'
+    botaoMenos.style.width = '30px'
+    botaoMenos.style.marginLeft = '20px'
 
-  const br = document.createElement("br")
-  titulo.parentNode.insertBefore(br, titulo.nextSibling)
-  titulo.parentNode.insertBefore(botaoDiminuir, titulo.nextSibling)
-  titulo.parentNode.insertBefore(botaoAumentar, titulo.nextSibling)
-  
-  const botao = document.querySelectorAll('#botao')
-  for(let i = 0; i < 2; i++){
-    botao[i].style.borderRadius = '10px'
-    botao[i].style.width = '30px'
-    botao[1].style.marginLeft = '20px'
-  }
-}
-function clicou(btn) {
-  if (btn == "mais" && tamanho < 6) {
-    tamanho += 0.5
-  } else if(btn == 'menos' && tamanho > 1) {
-    tamanho -= 0.5
-  }
-  titulo.style.fontSize = `${tamanho}em`
-}
+    const br = document.createElement('br') 
+
+    titulo.parentNode.insertBefore(br, titulo.nextSibling)
+    titulo.parentNode.insertBefore(botaoMenos, titulo.nextSibling)
+    titulo.parentNode.insertBefore(botaoMais, titulo.nextSibling)
+
+    botaoMais.addEventListener('click', function() {
+        if (tam < 6) {
+            tam += 0.5
+        } 
+        titulo.style.fontSize = `${tam}em`
+    })
+    botaoMenos.addEventListener('click', function() { 
+        if (tam > 1) {
+            tam -= 0.5
+        }
+        titulo.style.fontSize = `${tam}em`
+    })
+} 
